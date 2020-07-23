9e985f269bfa3b9bcf89f149d0449ef1b2da2f0d
JoscaWij <Josca.W@gmx.net>
2020-07-23 10:49:36 +0200

    Add button navigation

---
+++ src/components/buttonNavigation/buttonNavigation.stories.js
@@ -0,0 +1,6 @@
+import "./buttonNavigation.css";
+import buttonNavigation from "./buttonNavigation.html";
+
+export default { title: "Navigation bottom" };
+
+export const mainNav = () => buttonNavigation;
