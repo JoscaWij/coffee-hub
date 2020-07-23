ab5075d49b5ba40204ce882924a9376c09b7a8c3
Josca Wijtenburg <68224111+JoscaWij@users.noreply.github.com>
2020-07-23 13:56:15 +0200

    Add Bottom Navigation (#8)
    * Add button navigation
    
    * Add css
    
    * Fix bottom nav name and css
    
    * Fix alignment icons
    
    * Fix width von nav
    
    * Fix opacity of icons in nav
    
    * Switch SVGs with images
    
    * Badge in comments
    
    * Badge
    
    * Add position relative to navigation items

---
+++ src/components/buttonNavigation/bottomNavigation.stories.js
@@ -0,0 +1,6 @@
+import "./bottomNavigation.css";
+import bottomNavigation from "./bottomNavigation.html";
+
+export default { title: "Navigation bottom" };
+
+export const mainNav = () => bottomNavigation;
