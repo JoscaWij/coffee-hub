acebc29054c910c4f892f66bac643fe6ef60ac81
JoscaWij <Josca.W@gmx.net>
2020-07-23 11:33:59 +0200

    Fix bottom nav name and css

--- src/components/buttonNavigation/buttonNavigation.stories.js
+++
@@ -1,6 +0,0 @@
-import "./buttonNavigation.css";
-import buttonNavigation from "./buttonNavigation.html";
-
-export default { title: "Navigation bottom" };
-
-export const mainNav = () => buttonNavigation;
