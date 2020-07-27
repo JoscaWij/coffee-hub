51781be91325e57ecdbe1c273fc0eed33839941f
JoscaWij <Josca.W@gmx.net>
2020-07-27 19:45:37 +0200

    Add decrease and increase quantity funktion with btn-click

--- src/components/quantityInput/quantityInput.stories.js
+++ src/components/quantityInput/quantityInput.stories.js
@@ -1,6 +1,30 @@
 import "./quantityInput.css";
 import quantityInput from "./quantityInput.html";
+import { useEffect } from "@storybook/client-api";
 
-export default { title: "Quantity Input" };
+export default { title: "Quantity Selector" };
 
-export const coffeeSelector = () => quantityInput;
+export const coffeeSelector = () => {
+  useEffect(() => {
+    // Select buttons here
+    const btnMinus = document.querySelector(".quantityInput_btnMinus");
+    const quantityOutput = document.querySelector(".quantityOutput");
+    const btnPlus = document.querySelector(".quantityInput_btnPlus");
+    btnMinus.addEventListener("click", () => {
+      const oldQuantity = Number(quantityOutput.innerHTML);
+      if (oldQuantity === 2) {
+        btnMinus.disabled = true;
+      }
+      quantityOutput.innerHTML = oldQuantity - 1;
+    });
+    btnPlus.addEventListener("click", () => {
+      const oldQuantity = Number(quantityOutput.innerHTML);
+      if (oldQuantity === 1) {
+        btnMinus.disabled = false;
+      }
+      quantityOutput.innerHTML = oldQuantity + 1;
+    });
+  });
+
+  return quantityInput;
+};
