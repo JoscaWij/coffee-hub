41ea540e7e3d4bc6c14f8bed34b22c7f791a0a1c
JoscaWij <Josca.W@gmx.net>
2020-07-28 16:29:16 +0200

    Outsorce button.js with funktion createButton

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,19 +1,9 @@
 import "./button.css";
+import { createButton } from "./button";
 
 export default { title: "Button" };
 
-function createButton(buttonText) {
-  const button = document.createElement("button");
-  button.className = "btn";
-  const text = document.createTextNode(buttonText);
-  button.append(text);
-  return button;
-}
-
 export const addToCart = () => {
   const button = createButton("Add to cart");
-  button.addEventListener("click", () => {
-    alert("Thanks for shopping");
-  });
   return button;
 };
