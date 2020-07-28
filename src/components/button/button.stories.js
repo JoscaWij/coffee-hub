c253a283f162d83f5ae2315c6e330bfc36aa52d0
JoscaWij <Josca.W@gmx.net>
2020-07-28 16:59:04 +0200

    Delete button.stories

--- src/components/button/button.stories.js
+++
@@ -1,9 +0,0 @@
-import "./button.css";
-import { createButton } from "./button";
-
-export default { title: "Button" };
-
-export const addToCart = () => {
-  const button = createButton("Add to cart");
-  return button;
-};
