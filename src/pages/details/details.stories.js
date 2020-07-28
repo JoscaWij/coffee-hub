65e3ac84fd3d93cce773e61c4fb8b73dda35332f
JoscaWij <Josca.W@gmx.net>
2020-07-28 19:40:27 +0200

    Add quantityInput to details-page

--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -1,12 +1,13 @@
 // import { useEffect } from "@storybook/client-api";
 import { createButton } from "../../components/button/button";
+import { createQuantityInput } from "../../components/quantityInput/quantityInput";
 
 export default { title: "Pages/Details" };
 
 export const basic = () => {
   const main = document.createElement("main");
-  // const quantitySelector = createQuantitySelector();
-  // main.append(quantitySelector);
+  const quantitySelector = createQuantityInput();
+  main.append(quantitySelector);
   const button = createButton("Add to cart");
   main.append(button);
 
