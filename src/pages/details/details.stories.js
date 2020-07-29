86d82bb152a68a0e59be38068760b86f94b2552c
JoscaWij <Josca.W@gmx.net>
2020-07-29 15:16:25 +0200

    Add details.css

--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -1,6 +1,7 @@
 // import { useEffect } from "@storybook/client-api";
 import { createButton } from "../../components/button/button";
 import { createQuantityInput } from "../../components/quantityInput/quantityInput";
+import "./details.css";
 
 export default { title: "Pages/Details" };
 
