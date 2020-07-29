30dc7bad978a81210432da55dd37c962fed0202d
JoscaWij <Josca.W@gmx.net>
2020-07-29 11:49:49 +0200

    Add createElement function in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,7 @@
-// utilities 
+// utilities
 
+export const createElement = (tagName, props) => {
+  const element = document.createElement(tagName);
+  Object.assign(element, props);
+  return element;
+};
