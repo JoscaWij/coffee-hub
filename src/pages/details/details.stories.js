e9b5e55edb5942a75474a85752bea379e0525b0d
JoscaWij <Josca.W@gmx.net>
2020-07-29 15:33:52 +0200

    Add basic stucture main and header, import svg

--- src/pages/details/details.stories.js
+++ src/pages/details/details.stories.js
@@ -2,15 +2,30 @@
 import { createButton } from "../../components/button/button";
 import { createQuantityInput } from "../../components/quantityInput/quantityInput";
 import "./details.css";
+import { createElement } from "../../utils";
+import macciatoSrc from "../../assets/macciato.svg";
 
 export default { title: "Pages/Details" };
 
 export const basic = () => {
-  const main = document.createElement("main");
+  const container = createElement("div");
+  const header = createElement("header");
+  const title = createElement("h2", {
+    innerHTML: "Macciato",
+  });
+  const macciatoImg = createElement("img", {
+    src: macciatoSrc,
+  });
+  const form = createElement("form");
   const quantitySelector = createQuantityInput();
-  main.append(quantitySelector);
   const button = createButton("Add to cart");
-  main.append(button);
 
-  return main;
+  container.append(header);
+  container.append(form);
+  header.append(title);
+  header.append(macciatoImg);
+  form.append(quantitySelector);
+  form.append(button);
+
+  return container;
 };
