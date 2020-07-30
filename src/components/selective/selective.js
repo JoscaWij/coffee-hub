0181a4fc02890d038237c6005c250f2633b4ac6a
JoscaWij <Josca.W@gmx.net>
2020-07-30 15:27:04 +0200

    Add images

--- src/components/selective/selective.js
+++ src/components/selective/selective.js
@@ -1,18 +1,34 @@
 import "./selective.css";
 import { createElement } from "../../utils";
+import espressoSrc from "../../assets/Espresso.svg";
+import cappuccionoSrc from "../../assets/Cappuccino.svg";
+import macciatoSrc from "../../assets/macciato.svg";
+import mochaSrc from "../../assets/Mocha.svg";
+import latteSrc from "../../assets/latte.svg";
 
 export function createSelection() {
   const selection = createElement("ul", {
     className: "selective",
   });
 
-  const coffees = ["Espresso", "Cappuccino", "Macciato", "Mocha", "Latte"];
+  const coffees = [
+    { innerText: "Espresso", src: espressoSrc },
+    { innerText: "Cappuccino", src: cappuccionoSrc },
+    { innerText: "Macciato", src: macciatoSrc },
+    { innerText: "Mocha", src: mochaSrc },
+    { innerText: "Latte", src: latteSrc },
+  ];
 
-  coffees.forEach((titel) => {
+  coffees.forEach((coffee) => {
     const listItem = createElement("li", {
-      innerText: titel,
+      innerText: coffee.innerText,
     });
+    const img = createElement("img", {
+      src: coffee.src,
+    });
+    listItem.prepend(img);
     selection.append(listItem);
   });
+
   return selection;
 }
